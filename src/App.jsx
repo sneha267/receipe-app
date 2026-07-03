7da03300832d8ac95c0cf6d6668fed4fb2f02aa0
sneha267 <kadamsneha634@gmail.com>
2026-07-03 20:38:56 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,41 @@ import RecipeDetailView from "./components/RecipeDetailView.jsx"
 import SearchView from "./components/SearchView.jsx";
 import Cuisines from "./components/Cuisines.jsx";
 import HomeView from "./components/HomeView.jsx";
-// import SearchView from "./components/SearchView.jsx";
+import SearchView from "./components/SearchView.jsx";
 // import Navbar from "./components/Navbar.jsx";
+const API_URL = "https://www.themealdb.com/api/json/v1/1/";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 const App = () => {
+  const [searchQuery,setSearchQuery]=useState("")
+  const [searchResult,setSearchResult]=useState([])
+  const [searchLoading,setSearchLoading]=useState(false)
+  const handleSearch = useCallback(async (query)=>{
+    setSearchQuery(query)
+    setSearchResult([])
+    setSearchLoading(true)
+    try {
+            
+            const res= await fetch(`${API_URL}search.php?s=${query}`)
+            if(!res.ok) throw new Error(`Error: ${res.status}`);
+            const result = await res.json();
+            setSearchResult(result?.meals || [])
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setSearchLoading(false)
+        }
+  },[])
   return (
     <>
       <Router>
         <div className="min-h-screen bg-gray-950 font-sans text-gray-100">
-          <Navbar />
+          <Navbar handleSearch={handleSearch}/>
           <Cuisines />
           <Routes>
             <Route path="/" element={<HomeView />} />
             <Route path="/recipe/:id" element={<RecipeDetailView />} />
+            <Route path="/search/:query" element={<SearchView meal={searchResult}
+            loading={searchLoading}/>} />
           </Routes>
         </div>
       </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,48 @@
 import React , {useState} from 'react'
 import {Search, ChefHat } from "lucide-react";
 import {Link} from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({handleSearch}) => {
     const [input, setInput]= useState("")
-    const handleSearch =(e)=>{
+    const searchHandler =(e)=>{
         e.preventDefault();
         console.log("searching")
         setInput("")
     }
   return (
     <>
-    <nav className="sticky to-0 z-50 bg-gray-950/90 backdrop-blur-md shadow-2xl shadow-black/50 border-b border-blue-900/50">
+      <nav className="sticky to-0 z-50 bg-gray-950/90 backdrop-blur-md shadow-2xl shadow-black/50 border-b border-blue-900/50">
         <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between items-center h-16">
-                <Link to={"/"} className="flex items-center font-black text-white hover:text-blue-400 transition duration-300 tracking-widest">
-                <ChefHat className="w-7 h-7 mr-2 text-yellow-400 fill-yellow-400/20"/>
-                <span className="text-blue-400">Pro</span>Chef
-                </Link>
-                <form onSubmit={handleSearch} className="flex-1 max-w-lg mx-4 hidden sm:flex">
-                    <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Search dishes, ingredients or cuisines ..." className="w-full px-5 py-2 border border-gray-700 bg-gray-900 text-gray-500 rounded-l-full focus:outline-none focus:ring-4 focus:ring-blue-600/50 transition placeholder-gray-500 shadow-inner shadow-black/50"/>
-                    <button type="submit" className="bg-linear-to-r from-blue-600 to-cyan-500 text-white p-2.5 rounded-r-full hover:from-blue-700 hover:to-cyan-600 transition duration-300 shadow-lg shadow-blue-800/50 hover:shadow-xl hover:shadow-blue-800/90">
-                        <Search className="w-5 h-5"/>
-                    </button>
-                </form>
-            </div>
-        </div>  
-    </nav>
+          <div className="flex justify-between items-center h-16">
+            <Link
+              to={"/"}
+              className="flex items-center font-black text-white hover:text-blue-400 transition duration-300 tracking-widest"
+            >
+              <ChefHat className="w-7 h-7 mr-2 text-yellow-400 fill-yellow-400/20" />
+              <span className="text-blue-400">Pro</span>Chef
+            </Link>
+            <form
+              onSubmit={searchHandler}
+              className="flex-1 max-w-lg mx-4 hidden sm:flex"
+            >
+              <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Search dishes, ingredients or cuisines ..."
+                className="w-full px-5 py-2 border border-gray-700 bg-gray-900 text-gray-500 rounded-l-full focus:outline-none focus:ring-4 focus:ring-blue-600/50 transition placeholder-gray-500 shadow-inner shadow-black/50"
+              />
+              <button
+                type="submit"
+                className="bg-linear-to-r from-blue-600 to-cyan-500 text-white p-2.5 rounded-r-full hover:from-blue-700 hover:to-cyan-600 transition duration-300 shadow-lg shadow-blue-800/50 hover:shadow-xl hover:shadow-blue-800/90"
+              >
+                <Search className="w-5 h-5" />
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar
